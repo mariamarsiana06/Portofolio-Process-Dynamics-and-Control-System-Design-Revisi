3b1b3264185af087f84d002d342469ecc37f135b
mariamarsiana06 <cacadut0106@gmail.com>
2026-06-17 13:21:56 +0700

    Add files via upload

---
+++ process.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const fadeElements = document.querySelectorAll('.fade-in');
+
+    const appearOptions = {
+        threshold: 0.15,
+        rootMargin: "0px 0px -50px 0px"
+    };
+
+    const appearOnScroll = new IntersectionObserver(function(entries, observer) {
+        entries.forEach(entry => {
+            if (!entry.isIntersecting) {
+                return;
+            } else {
+                entry.target.classList.add('visible');
+                observer.unobserve(entry.target); 
+            }
+        });
+    }, appearOptions);
+
+    fadeElements.forEach(element => {
+        appearOnScroll.observe(element);
+    });
+});
